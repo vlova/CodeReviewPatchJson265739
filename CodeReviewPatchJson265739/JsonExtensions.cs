620943b08e408e40bf21460bb22169f711037fe0
vlova <nsinreal+vlova@gmail.com>
2021-08-12 21:39:22 +0300

    Initial commit based on https://codereview.stackexchange.com/questions/265739/patch-a-json-object-using-dynamic-expandoobject-with-system-text-json

---
+++ CodeReviewPatchJson265739/JsonExtensions.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text.Json;
+
+namespace CodeReviewPatchJson265739
+{
+    public static class JsonExtensions
+    {
+        public static JsonElement DynamicUpdate(
+            this IDictionary<string, object> entity,
+            string patchJson,
+            bool addPropertyIfNotExists = false,
+            bool useTypeValidation = true,
+            JsonDocumentOptions options = default)
+        {
+            using JsonDocument doc = JsonDocument.Parse(patchJson, options);
+            return DynamicUpdate(entity, doc, addPropertyIfNotExists, useTypeValidation, options);
+        }
+
+        public static JsonElement DynamicUpdate(
+            this IDictionary<string, object> entity,
+            JsonDocument doc,
+            bool addPropertyIfNotExists = false,
+            bool useTypeValidation = true,
+            JsonDocumentOptions options = default)
+        {
+            if (doc == null) throw new ArgumentNullException(nameof(doc));
+            if (doc.RootElement.ValueKind != JsonValueKind.Object)
+                throw new NotSupportedException("Only objects are supported.");
+
+            foreach (JsonProperty jsonProperty in doc.RootElement.EnumerateObject())
+            {
+                string propertyName = jsonProperty.Name;
+                JsonElement newElement = doc.RootElement.GetProperty(propertyName);
+                bool hasProperty = entity.TryGetValue(propertyName, out object oldValue);
+
+                // sanity checks
+                JsonElement? oldElement = null;
+                if (oldValue != null)
+                {
+                    if (!oldValue.GetType().IsAssignableTo(typeof(JsonElement)))
+                        throw new ArgumentException($"Type mismatch. Must be {nameof(JsonElement)}.", nameof(entity));
+                    oldElement = (JsonElement)oldValue;
+                }
+                if (!hasProperty && !addPropertyIfNotExists) continue;
+                entity[propertyName] = GetNewValue(
+                    oldElement, newElement, propertyName,
+                    addPropertyIfNotExists, useTypeValidation, options);
+            }
+            using JsonDocument finalDoc = JsonDocument.Parse(JsonSerializer.Serialize(entity));
+            return finalDoc.RootElement.Clone();
+        }
+        private static JsonElement GetNewValue(
+            JsonElement? oldElementNullable,
+            JsonElement newElement,
+            string propertyName,
+            bool addPropertyIfNotExists,
+            bool useTypeValidation,
+            JsonDocumentOptions options)
+        {
+            if (oldElementNullable == null) return newElement.Clone();
+            JsonElement oldElement = (JsonElement)oldElementNullable;
+
+            // type validation
+            if (useTypeValidation && !IsValidType(oldElement, newElement))
+                throw new ArgumentException($"Type mismatch. The property '{propertyName}' must be of type '{oldElement.ValueKind}'.", nameof(newElement));
+
+            // recursively go down the tree for objects
+            if (oldElement.ValueKind == JsonValueKind.Object)
+            {
+                string oldJson = oldElement.GetRawText();
+                string newJson = newElement.ToString();
+                IDictionary<string, object> entity = JsonSerializer.Deserialize<ExpandoObject>(oldJson);
+                return DynamicUpdate(entity, newJson, addPropertyIfNotExists, useTypeValidation, options);
+            }
+
+            return newElement.Clone();
+        }
+
+        private static bool IsValidType(JsonElement oldElement, JsonElement newElement)
+        {
+            if (newElement.ValueKind == JsonValueKind.Null) return true;
+
+            // 'true' --> 'false'
+            if (oldElement.ValueKind == JsonValueKind.True && newElement.ValueKind == JsonValueKind.False) return true;
+            // 'false' --> 'true'
+            if (oldElement.ValueKind == JsonValueKind.False && newElement.ValueKind == JsonValueKind.True) return true;
+
+            // type validation
+            return (oldElement.ValueKind == newElement.ValueKind);
+        }
+
+        private static bool IsValidJsonPropertyName(string value)
+        {
+            if (string.IsNullOrEmpty(value)) return false;
+
+            // this is validation for our specific use case (C#)
+            // note that the official docs don't prohibit this though.
+            // https://datatracker.ietf.org/doc/html/rfc7159
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (char.IsLetterOrDigit(value[i])) continue;
+                switch (value[i])
+                {
+                    case '-':
+                    case '_':
+                    default:
+                        break;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ CodeReviewPatchJson265739/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Dynamic;
+using System.Text.Json;
+
+namespace CodeReviewPatchJson265739
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string original = @"{""foo"":[1,2,3],""parent"":{""childInt"":1},""bar"":""example""}";
+            string patch = @"{""foo"":[9,8,7],""parent"":{""childInt"":9,""childString"":""woot!""},""bar"":null}";
+            Console.WriteLine(original);
+
+            // change this value to see the different types of patching method
+            bool addPropertyIfNotExists = false;
+
+            ExpandoObject expandoObject = JsonSerializer.Deserialize<ExpandoObject>(original);
+
+            // patch it!
+            expandoObject.DynamicUpdate(patch, addPropertyIfNotExists);
+            Console.WriteLine(JsonSerializer.Serialize(expandoObject));
+        }
+    }
+}
